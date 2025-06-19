551c8580181e4e962cb324c5ad61055c7454c390
TestAccount930 <Sion.Lynch@liverpool.ac.uk>
2025-06-19 14:14:22 +0100

    Update create-plot.R

--- R/create-plot.R
+++ R/create-plot.R
@@ -17,7 +17,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "blue") # We have changed the plot colour to blue
+  geom_point(colour = "cyan") # In main this is Cyan
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
